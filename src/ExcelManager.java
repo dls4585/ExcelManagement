5b00dfb9c551c444fde69890a668ae1085a23062
dls4585 <dls4585@naver.com>
2021-09-20 22:30:41 +0900

    JList 구현, 선택 값 넘기는 것 구현

--- src/ExcelManager.java
+++ src/ExcelManager.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -20,21 +22,38 @@ public class ExcelManager {
 
         }
     }
-
+    public static Set<String> getCampaigns() {
+        return campaigns.keySet();
+    }
     void initFile() { // 처음 캠페인 로딩
         try {
             campaigns  = new HashMap<>();
             FileInputStream file = new FileInputStream(fileName);
             XSSFWorkbook workbook = new XSSFWorkbook(file);
             XSSFSheet sheet = workbook.getSheetAt(0);
+            for (Row row : sheet) {
+                if(row.getRowNum() < 6) continue;
+                if(row.getCell(0) == null) break;
 
+                String campaignName = row.getCell(3).getStringCellValue();
+                String group = row.getCell(7).getStringCellValue();
+                if(!campaigns.containsKey(campaignName)) {
+                    String AFCampaignName = row.getCell(4).getStringCellValue();
+                    String AFChannelName = row.getCell(5).getStringCellValue();
+                    Campaign newCampaign = new Campaign(campaignName, AFCampaignName, AFChannelName);
+                    newCampaign.groups.add(group);
+                    campaigns.put(campaignName, newCampaign);
 
-
+                } else {
+                    campaigns.get(campaignName).groups.add(group);
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
+
+
 }
 
 class Campaign {
--- src/FileDialogTest.java
+++ src/FileDialogTest.java
@@ -1,21 +1,26 @@
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
+import java.util.Set;
 
 
 public class FileDialogTest extends JFrame implements ActionListener {
     JButton btnLoad, btnInitial, btnText;
     JPanel buttons, textPanel;
     JTextArea textArea;
+    List<String> selected;
     public FileDialogTest() {
         setTitle("FileDiaog");
         InitLayout();
 
     }
 
-    public void InitLayout() {
+    private void InitLayout() {
         setSize(600, 700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -23,14 +28,14 @@ public class FileDialogTest extends JFrame implements ActionListener {
         buttons.setLayout(null);
         buttons.setBounds(0, 0, 600, 100);
 
-        btnLoad = new JButton("만들고 싶은 링크 정보 파일");
-        btnLoad.addActionListener(this);
-        btnLoad.setBounds(300, 25, 250, 50);
-        buttons.add(btnLoad);
+//        btnLoad = new JButton("만들고 싶은 링크 정보 파일");
+//        btnLoad.addActionListener(this);
+//        btnLoad.setBounds(300, 25, 250, 50);
+//        buttons.add(btnLoad);
 
         btnInitial = new JButton("처음 파일 넣기");
         btnInitial.addActionListener(this);
-        btnInitial.setBounds(50, 25, 250, 50);
+        btnInitial.setBounds(50, 25, 500, 50);
         buttons.add(btnInitial);
 
         textPanel = new JPanel();
@@ -50,7 +55,41 @@ public class FileDialogTest extends JFrame implements ActionListener {
 
         setVisible(true);
     }
+    private void makeSecondFrame() {
+        JFrame frame2 = new JFrame();
+        frame2.setTitle("캠페인 선택");
+        frame2.setSize(600, 700);
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBounds(0, 0, 600, 700);
+
+        Set<String> campaigns = ExcelManager.getCampaigns();
+
+        JList campaignList = new JList(campaigns.toArray());
+        campaignList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        JScrollPane scroll = new JScrollPane(campaignList);
+        scroll.setBounds(0,0, 600, 600);
+        panel.add(scroll);
+
+        JButton button = new JButton("확인");
+
+        button.addActionListener(e -> {
+            selected = new ArrayList<>();
+            for (Object o : campaignList.getSelectedValuesList()) {
+                selected.add((String) o);
+            }
+            // Frame1(this)의 TextArea 에 값 넣기
+            frame2.dispose();
+        });
+        button.setBounds(475, 610, 75, 50);
+        panel.add(button);
+
+        frame2.add(panel);
+        frame2.setVisible(true);
+
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource().equals(btnLoad)) {
@@ -81,6 +120,8 @@ public class FileDialogTest extends JFrame implements ActionListener {
             String text = textArea.getText();
             String[] lines = text.split("\n");
             System.out.println(Arrays.toString(lines));
+            makeSecondFrame();
+
         }
     }
 }
